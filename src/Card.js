79ed7e5efc0d90a0881a32b3e13fa1391b88a693
Raufnarejo505 <raufnarejo13@gmail.com>
2022-10-02 08:16:12 +0500

    attached robohash api in order to display images of kitten

---
+++ src/Card.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Card() {
+  return (
+    <div>
+      <img alt="robo" src="https://robohash.org/test01?set=set4"  />
+      <div>
+        <h2>Robot name</h2>
+        <p>Robot email</p>
+      </div>
+      
+    </div>
+  )
+}
